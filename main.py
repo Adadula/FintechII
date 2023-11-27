e40e36d4a593ccae17ae5eda309d73627abefb77
Adadula <151517537+Adadula@users.noreply.github.com>
2023-11-27 16:17:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import mod
+mod.psum(3,4)
+mod.pmultiply(5,4)
+
+import platform
+print(dir(platform))
---
+++ mod.py
@@ -0,0 +1,12 @@
+# In mod.py, create a function psum(number1, number2) that takes two arguments and prints the sum of them. Assume the input arguments to be numbers.
+# In mod.py, create a function pmultiply(number1, number2) that takes two arguments and prints the product of them. Assume the input arguments to be numbers.
+
+def psum(number1, number2):
+    int(number1)
+    int(number2)
+    print("The sum of numbers is: ", number1+number2)
+
+def pmultiply(number1, number2):
+    int(number1)
+    int(number2)
+    print("The product of numbers is: ", number1*number2)
