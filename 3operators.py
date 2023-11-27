b60659245c0c908c9d98fdbeb28dba85b7034222
Adadula <151517537+Adadula@users.noreply.github.com>
2023-11-27 14:50:20 +0000

    Add files via upload

---
+++ 3operators.py
@@ -0,0 +1,11 @@
+#3operators.py
+
+# 3.1 Operators
+
+# 3.1.1 Multiply 4 with 8, and print the result.
+# TODO: Write your code below
+print("the result is:", 4+8)
+
+# 3.1.2 Divide 30 by 6, and print the result.
+# TODO: Write your code below
+print("the result is:", 30/6)
